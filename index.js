01f2f1d5b6b0ef2218fe1cb19325ca4d32585ab3
David <dsem562@gmail.com>
2022-03-12 13:32:52 -0800

    Completed assignment 2.  Please check prior commit, as I was able to make npm test pass with that version but, this one will not pass all tests(I believe this version is most pertinent to lesson).

--- index.js
+++ index.js
@@ -6,10 +6,6 @@ function introductionWithLanguage(name, language) {
     return `Hi, my name is ${name} and I am learning to program in ${language}.`;
 }
 
-function introductionWithLanguageOptional(name, language) {    
-    if (name && language) {
-        return `Hi, my name is ${name} and I am learning to program in ${language}.`;
-    } else if (name) {
-        return `Hi, my name is ${name} and I am learning to program in JavaScript.`;
-}
+function introductionWithLanguageOptional(name, language = "Javascript") {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;   
 }
